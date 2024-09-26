4688d60a4e8f06cbe6a426ab4a245da35333fd88
kahyun0255 <kkh9946@naver.com>
2024-09-26 15:32:43 +0900

    Fix: 가계부 목록 상세 조회 url 수정

--- src/main/java/com/grepp/nbe1_2_team09/controller/finance/AccountBookController.java
+++ src/main/java/com/grepp/nbe1_2_team09/controller/finance/AccountBookController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,8 +40,8 @@ public class AccountBookController {
     }
 
     //가계부 목록 상세 조회
-    @GetMapping("/{expenseId}")
-    public AccountBookOneResp findAccountBook(@PathVariable Long expenseId) {
+    @GetMapping
+    public AccountBookOneResp findAccountBook(@RequestParam Long expenseId) {
         return accountBookService.findAccountBook(expenseId);
     }
 
