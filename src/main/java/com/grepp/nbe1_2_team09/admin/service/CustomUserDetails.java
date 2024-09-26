2294949a9de766eec7b0d58a52b9aa41308b6992
ooni <gdbsrjsdn@gmail.com>
2024-09-26 11:58:05 +0900

    Feature: 로그인 시 DB 사용자 존재여부 확인 UserDetails 클래스 작성

---
+++ src/main/java/com/grepp/nbe1_2_team09/admin/service/CustomUserDetails.java
@@ -0,0 +1,60 @@
+package com.grepp.nbe1_2_team09.admin.service;
+
+import com.grepp.nbe1_2_team09.admin.dto.CustomUserInfoDTO;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final CustomUserInfoDTO user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<String> roles = new ArrayList<>();
+        roles.add(user.getRole().toString());
+
+        return roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserId().toString();
+    }
+}
