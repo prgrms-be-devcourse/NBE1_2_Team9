243bddb4aa43dda61db89218a810746f079baa3c
kahyun0255 <kkh9946@naver.com>
2024-09-30 16:09:49 +0900

    Fix: 환율 컨트롤러에서 DTO 전송하도록 수정

--- src/main/java/com/grepp/nbe1_2_team09/controller/finance/ExchangeRateController.java
+++ src/main/java/com/grepp/nbe1_2_team09/controller/finance/ExchangeRateController.java
@@ -1,7 +1,9 @@
 package com.grepp.nbe1_2_team09.controller.finance;
 
 import com.grepp.nbe1_2_team09.controller.finance.dto.ExchangeRateReqDTO;
+import com.grepp.nbe1_2_team09.controller.finance.dto.ExchangeRateResDTO;
 import com.grepp.nbe1_2_team09.domain.service.finance.ExchangeRateService;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,10 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExchangeRateController {
 
     private final ExchangeRateService exchangeRateService;
+    private LocalDateTime exchangeTime;
 
     @GetMapping
-    public void exchangeRate(@RequestBody ExchangeRateReqDTO exchangeRateReqDTO){
-        exchangeRateService.exchangeRate(exchangeRateReqDTO);
+    public ExchangeRateResDTO exchangeRate(@RequestBody ExchangeRateReqDTO exchangeRateReqDTO){
+        ExchangeRateResDTO result= exchangeRateService.exchangeRate(exchangeRateReqDTO);
+        result.setTime(exchangeTime);
+        return result;
     }
 
 }
--- src/main/java/com/grepp/nbe1_2_team09/domain/repository/finance/ExchangeRateRepository.java
+++ src/main/java/com/grepp/nbe1_2_team09/domain/repository/finance/ExchangeRateRepository.java
@@ -2,6 +2,7 @@ package com.grepp.nbe1_2_team09.domain.repository.finance;
 
 import com.grepp.nbe1_2_team09.domain.entity.ExchangeRate;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
