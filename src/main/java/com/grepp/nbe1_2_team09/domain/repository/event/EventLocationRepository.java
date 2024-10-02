e0809a27b09547818c6c0390f025eca53005f106
wjdwltn <jlsowltn20000@gmail.com>
2024-10-02 11:43:40 +0900

    Feat: 선택한 날짜와 모든 장소들 순서대로 가져오는 기능

--- src/main/java/com/grepp/nbe1_2_team09/domain/repository/event/EventLocationRepository.java
+++ src/main/java/com/grepp/nbe1_2_team09/domain/repository/event/EventLocationRepository.java
@@ -4,12 +4,17 @@ import com.grepp.nbe1_2_team09.domain.entity.event.Event;
 import com.grepp.nbe1_2_team09.domain.entity.event.EventLocation;
 import com.grepp.nbe1_2_team09.domain.entity.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 public interface EventLocationRepository extends JpaRepository<EventLocation, Long> {
     List<EventLocation> findByEvent(Event event);
+
+    @Query("SELECT el from EventLocation el WHERE el.event=:event AND DATE(el.visitStartTime) =:date ORDER BY el.visitStartTime ASC")
+    List<EventLocation> findByEventAndDateOrderByTime(Event event, LocalDate date);
 
     Optional<EventLocation> findByEventAndLocation(Event event, Location location);
 }
