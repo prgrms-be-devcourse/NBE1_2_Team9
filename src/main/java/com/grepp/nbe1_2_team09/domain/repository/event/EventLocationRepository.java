a536ceed22ba920e2e1cbfa5a12978b9c873e473
wjdwltn <jlsowltn20000@gmail.com>
2024-10-02 10:10:16 +0900

    Feat: 같은 일정 해당 날짜에 포함된 장소 시간 순서대로

--- src/main/java/com/grepp/nbe1_2_team09/domain/repository/event/EventLocationRepository.java
+++ src/main/java/com/grepp/nbe1_2_team09/domain/repository/event/EventLocationRepository.java
@@ -3,13 +3,20 @@ package com.grepp.nbe1_2_team09.domain.repository.event;
 import com.grepp.nbe1_2_team09.domain.entity.event.Event;
 import com.grepp.nbe1_2_team09.domain.entity.event.EventLocation;
 import com.grepp.nbe1_2_team09.domain.entity.Location;
+import org.springframework.cglib.core.Local;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 public interface EventLocationRepository extends JpaRepository<EventLocation, Long> {
     List<EventLocation> findByEvent(Event event);
+
+    @Query("SELECT el from EventLocation el WHERE el.event=:event AND DATE(el.visitStartTime) =:date ORDER BY el.visitStartTime ASC")
+    List<EventLocation> findByEventAndDateOrderByTime(Event event, LocalDate date);
 
     Optional<EventLocation> findByEventAndLocation(Event event, Location location);
 }
