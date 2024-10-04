b968c4b0a2c86ac53f4b15fbb25612c0db654b82
wjdwltn <jlsowltn20000@gmail.com>
2024-10-04 10:47:53 +0900

    Feat: 나라별 도시 검색 기능 추가+프론트

--- src/main/java/com/grepp/nbe1_2_team09/controller/city/CityApiController.java
+++ src/main/java/com/grepp/nbe1_2_team09/controller/city/CityApiController.java
@@ -1,6 +1,6 @@
 package com.grepp.nbe1_2_team09.controller.city;
 
-import com.grepp.nbe1_2_team09.controller.city.dto.SearchCityResponse;
+import com.grepp.nbe1_2_team09.controller.city.dto.CityResponse;
 import com.grepp.nbe1_2_team09.domain.service.city.CityApiService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -15,10 +15,17 @@ public class CityApiController {
 
     private final CityApiService cityService;
 
-    // 도시 또는 나라 검색
+    // 도시
     @GetMapping("/search")
-    public ResponseEntity<List<SearchCityResponse>> searchCities(@RequestParam(name = "input") String input) {
-        List<SearchCityResponse> results = cityService.searchCity(input);
+    public ResponseEntity<List<CityResponse>> searchCities(@RequestParam(name = "input") String input) {
+        List<CityResponse> results = cityService.searchCity(input);
         return ResponseEntity.ok(results);
+    }
+
+    //나라별 도시 검색
+    @GetMapping("/country")
+    public ResponseEntity<List<CityResponse>> getCitiesByCountry(@RequestParam String input) {
+        List<CityResponse> cities = cityService.getCitiesByCountry(input);
+        return ResponseEntity.ok(cities);
     }
 }
