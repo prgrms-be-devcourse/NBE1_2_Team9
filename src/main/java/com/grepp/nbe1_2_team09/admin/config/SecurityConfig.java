20d32e72f70249ea55d84c46db2ea67e7b714147
ooni <gdbsrjsdn@gmail.com>
2024-10-04 12:12:56 +0900

    Feature: OAuth2 로그인 핸들러 작성

--- src/main/java/com/grepp/nbe1_2_team09/admin/config/SecurityConfig.java
+++ src/main/java/com/grepp/nbe1_2_team09/admin/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.grepp.nbe1_2_team09.admin.config;
 import com.grepp.nbe1_2_team09.admin.jwt.JwtFilter;
 import com.grepp.nbe1_2_team09.admin.jwt.JwtUtil;
 import com.grepp.nbe1_2_team09.admin.service.CustomUserDetailsService;
+import com.grepp.nbe1_2_team09.admin.service.oauth2.OAuth2LoginFailureHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,12 +17,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final CustomUserDetailsService customUserDetailsService;
     private final JwtUtil jwtUtil;
+    private final OAuth2LoginFailureHandler oAuth2LoginFailureHandler;
 
     private static final String[] AUTH_WHITELIST = {
             "/swagger-ui/**", "/api-docs", "/user/**"
@@ -29,26 +32,31 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        // CSRF, CORS
-        httpSecurity.csrf((csrf) -> csrf.disable());
-        httpSecurity.cors((Customizer.withDefaults()));
+        httpSecurity
+                // CSRF, CORS
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors((Customizer.withDefaults()))
 
-        // 세션 관리 상태 없음으로 구성, Spring Security가 세션 생성 or 사용 X
-        httpSecurity.sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                // 세션 비활성화 (JWT 사용)
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
-        // FormLogin, BasicHttp 비활성화
-        httpSecurity.formLogin((form) -> form.disable());
-        httpSecurity.httpBasic(AbstractHttpConfigurer::disable);
+                // FormLogin, BasicHttp 비활성화
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
 
-        // JwtAuthFilter를 UsernamePasswordAuthenticationFilter 앞에 추가
-        httpSecurity.addFilterBefore(new JwtFilter(customUserDetailsService, jwtUtil), UsernamePasswordAuthenticationFilter.class);
+                .oauth2Login(oauth2 -> oauth2
+                        .failureHandler(oAuth2LoginFailureHandler)
+                )
 
-        // 권한 규칙 작성
-        httpSecurity.authorizeHttpRequests(authorize -> authorize
-                .requestMatchers(AUTH_WHITELIST).permitAll()
-                .anyRequest().permitAll()
-//                .anyRequest().authenticated()
-        );
+                // JwtAuthFilter를 UsernamePasswordAuthenticationFilter 앞에 추가
+                .addFilterBefore(new JwtFilter(customUserDetailsService, jwtUtil), UsernamePasswordAuthenticationFilter.class)
+
+                // 권한 규칙 작성
+                .authorizeHttpRequests(authorize -> authorize
+                                .requestMatchers(AUTH_WHITELIST).permitAll()
+                                .anyRequest().permitAll()
+//                                .anyRequest().authenticated()
+                );
 
         return httpSecurity.build();
     }
---
+++ src/main/java/com/grepp/nbe1_2_team09/admin/service/oauth2/OAuth2LoginFailureHandler.java
@@ -0,0 +1,24 @@
+package com.grepp.nbe1_2_team09.admin.service.oauth2;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OAuth2LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
+        log.error("로그인 실패: {}", exception.getMessage());
+
+        getRedirectStrategy().sendRedirect(request, response, "/users/signin");
+    }
+}
