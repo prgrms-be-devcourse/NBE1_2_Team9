b7934aa2b889f7f37b5129005ecd358564620154
LeeHyeonseob <hyeonseob22@gmail.com>
2024-10-08 10:36:22 +0900

    Fix : userId 말고 username을 FE로 전송하도록 수정

--- src/main/java/com/grepp/nbe1_2_team09/controller/group/dto/GroupMembershipDto.java
+++ src/main/java/com/grepp/nbe1_2_team09/controller/group/dto/GroupMembershipDto.java
@@ -3,12 +3,12 @@ package com.grepp.nbe1_2_team09.controller.group.dto;
 import com.grepp.nbe1_2_team09.domain.entity.group.GroupMembership;
 import com.grepp.nbe1_2_team09.domain.entity.group.GroupRole;
 
-public record GroupMembershipDto(Long id, Long groupId, Long userId, GroupRole role) {
+public record GroupMembershipDto(Long id, Long groupId, String username, GroupRole role) {
     public static GroupMembershipDto from(GroupMembership membership) {
         return new GroupMembershipDto(
                 membership.getMembershipId(),
                 membership.getGroup().getGroupId(),
-                membership.getUser().getUserId(),
+                membership.getUser().getUsername(),
                 membership.getRole()
         );
     }
