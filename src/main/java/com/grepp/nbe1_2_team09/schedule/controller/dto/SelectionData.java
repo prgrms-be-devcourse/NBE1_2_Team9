c9d7bb234cf0ec97c9a5d650c11cea221c56f950
wjdwltn <jlsowltn20000@gmail.com>
2024-10-09 13:25:52 +0900

    Fix

--- src/main/java/com/grepp/nbe1_2_team09/schedule/controller/dto/SelectionData.java
+++ src/main/java/com/grepp/nbe1_2_team09/schedule/controller/dto/SelectionData.java
@@ -3,5 +3,4 @@ package com.grepp.nbe1_2_team09.schedule.controller.dto;
 import java.io.Serializable;
 
 public record SelectionData(String date, String startTime, String endTime) implements Serializable {
-//
 }
